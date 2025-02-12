aa0573b20c51e6ed6a5a4c848a5fd4476deef048
Yuntae Jeon <67727745+yuntaeJ@users.noreply.github.com>
2025-02-12 14:26:56 +0900

    Update run_ffmpeg_oop_multi.py

--- Submissions/assignment3_2025-02-12/run_ffmpeg_oop_multi.py
+++ Submissions/assignment3_2025-02-12/run_ffmpeg_oop_multi.py
@@ -1,7 +1,7 @@
 """
 File Name: run_ffmpeg_oop_multi.py
 Created Date: 2025.02.11
-Programmer: Yuntae Jeon (refactored by ChatGPT)
+Programmer: Yuntae Jeon
 Description: ffmpeg-based RTSP streaming in OOP style with multiple windows
 """
 
